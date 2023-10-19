09caa016bd8c30d90e9a647ea3554d3146ee029b
SomEnaMeforme <vv12345v54321vv@gmail.com>
2023-10-19 22:51:37 +0500

    First task

--- src/test/java/edu/hw2/Task1Test.java
+++ src/test/java/edu/hw2/Task1Test.java
@@ -1,2 +1,75 @@
-package edu.hw2;public class Task1Test {
+package edu.hw2;
+
+import edu.hw1.EvenArrayUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import javax.management.OperationsException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static edu.hw2.Task1.Expr.Constant;
+import static edu.hw2.Task1.Expr.Addition;
+import static edu.hw2.Task1.Expr.Exponent;
+import static edu.hw2.Task1.Expr.Multiplication;
+import static edu.hw2.Task1.Expr.Negate;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class Task1Test {
+    @Test
+    @DisplayName("Для положительных входных значений, Negate возвращает отрицательное значение")
+    void negateFromPositiveConstReturnNegativeTest() {
+        var positiveConst = new Constant(4.5);
+        var negative = new Negate(positiveConst);
+        assertThat(negative.evaluate()).isEqualTo(-4.5);
+    }
+    @Test
+    @DisplayName("Для отрицательных входных значений Negate возвращает отрицательное значение")
+    void negateFromNegativeConstReturnNegativeTest() {
+        var negativeConst = new Constant(-4.5);
+        var negative = new Negate(negativeConst);
+        assertThat(negative.evaluate()).isEqualTo(-4.5);
+    }
+
+    @Test
+    @DisplayName("Negate работает и для переданных на вход double")
+    void inputToNegateMayBeDoubleTest() {
+        var negative = new Negate(64.8);
+        assertThat(negative.evaluate()).isEqualTo(-64.8);
+    }
+
+    @Test
+    @DisplayName("Тестирование входных значений для Multiplication")
+    void inputToMultiplicationTest() {
+        assertThat(new Multiplication(4, 0.8).evaluate()).isEqualTo(3.2);
+        assertThat(new Multiplication(new Constant(4), 0.8).evaluate()).isEqualTo(3.2);
+        assertThat(new Multiplication(4, new Constant(0.8)).evaluate()).isEqualTo(3.2);
+    }
+    @Test
+    @DisplayName("Тестирование входных значений для Addition")
+    void inputToAdditionTest() {
+        assertThat(new Addition(4, 0.8).evaluate()).isEqualTo(4.8);
+        assertThat(new Addition(new Constant(4), 0.8).evaluate()).isEqualTo(4.8);
+        assertThat(new Addition(4, new Constant(0.8)).evaluate()).isEqualTo(4.8);
+    }
+    @Test
+    @DisplayName("Тестирование входных значений для Addition")
+    void inputToExponentTest() {
+        assertThat(new Exponent(4, 1.5).evaluate()).isEqualTo(8);
+        assertThat(new Exponent(new Constant(4), 1.5).evaluate()).isEqualTo(8);
+        assertThat(new Exponent(4, new Constant(1.5)).evaluate()).isEqualTo(8);
+    }
+
+    @Test
+    @DisplayName("Тестирование входных значений для Addition")
+    void complicatedCorrectWorkTest() {
+        var two = new Constant(2);
+        var four = new Constant(4);
+        var negOne = new Negate(new Constant(1));
+        var sumTwoFour = new Addition(two, four);
+        var mult = new Multiplication(sumTwoFour, negOne);
+        var exp = new Exponent(mult, 2);
+        var res = new Addition(exp, new Constant(1));
+        assertThat(res.evaluate()).isEqualTo(37);
+    }
+
 }
