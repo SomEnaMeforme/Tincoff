9a8c77a272ef21efabf586e9ce18140076fa7bc1
SomEnaMeforme <vv12345v54321vv@gmail.com>
2023-10-23 01:15:07 +0500

    Refactor merge commit

--- src/main/java/edu/project1/Main.java
+++ src/main/java/edu/project1/Main.java
@@ -1,12 +1,13 @@
 package edu.project1;
 
-import edu.hw1.task0_task1.Task0;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public final class Main {
 
     private Main() {
     }
-
+    private final static Logger LOGGER = LogManager.getLogger();
     public static void main(String[] args) {
         LOGGER.info("Hello and welcome!");
         var game = Game.startGame();
